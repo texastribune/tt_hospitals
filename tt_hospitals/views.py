0a4f5ae691e8f459b6e4ba19125f07c78f9d4f19
Travis Swicegood <development@domain51.com>
2013-05-09 18:02:45 -0500

    Very, very simple nearby API

---
+++ tt_hospitals/views.py
@@ -0,0 +1,24 @@
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import D
+
+
+from tx_schools.api import ApiView, JSON
+from . import models
+
+
+class NearbyHospitalApiView(ApiView):
+    def get_content_data(self):
+        point = Point(float(self.request.GET['lat']),
+                float(self.request.GET['lng']))
+        distance = self.request.GET.get('distance', 5)
+        nearby = models.Hospital.objects.distance(point).filter(
+                coordinates__distance_lte=(point, D(mi=distance)))
+
+        data = []
+        for hospital in nearby:
+            data.append({
+                'name': unicode(hospital.name),
+                'distance': round(hospital.distance.mi, 2),
+                'geojson': JSON(hospital.coordinates.geojson),
+            })
+        return data
